ebf7a3a13f03d9c2d1e1ce55b6397accd4d04abe
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-08-19 20:38:01 +0200

    Bump everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello from cmkr!\n";
+int main() {
+#ifdef TEST_RELEASE
+  std::cout << "TEST_RELEASE is defined!\n";
+#else
+  std::cout << "TEST_RELEASE is not defined!\n";
+#endif
 }
